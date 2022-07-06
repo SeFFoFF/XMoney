096c022ff7a0e7d731d5f3d161df9a536f7bffcb
Maksym <67505897+nightmare017@users.noreply.github.com>
2022-07-06 18:32:16 +0300

    Server setup (#2)
    * feat: setup
    
    * remove file

---
+++ server/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './components/user/user.module';
+// import configuration from './config/configuration';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://admin:${'admin'}@cluster0.vqvyxhm.mongodb.net/?retryWrites=true&w=majority`,
+    ),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
